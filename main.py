9c8d9174cb8ef2d1737e0bc4051002f10e69154c
Nikita Serov <stud0000262775@study.utmn.ru>
2023-04-16 02:11:23 +0500

    now it parses more than 2000 vacancies

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import requests
 import json
 import time
-import os
+import datetime
 
-def getPage(page = 0):
+def getPage(date_from, date_to, page):
     # Справочник для параметров GET-запроса
     params = {
-        'text': 'DESCRIPTION:текст', # Текст фильтра. https://tyumen.hh.ru/article/1175 - параметры фильтрации
-        'area': 1342, # 95 - код Тюмени, 1342 - код Тюменской области
+        #'text': '', # Текст фильтра. https://tyumen.hh.ru/article/1175 - параметры фильтрации DESCRIPTION:текст
+        'area': 95, # 95 - код Тюмени, 1342 - код Тюменской области
         'page': page, # Индекс страницы поиска на HH
-        'per_page': 100 # Кол-во вакансий на 1 странице, больше 100 нельзя
+        'per_page': 100, # Кол-во вакансий на 1 странице, больше 100 нельзя
+        'date_from': date_from,
+        'date_to': date_to
     }
 
     req = requests.get('https://api.hh.ru/vacancies', params)
@@ -17,23 +19,49 @@ def getPage(page = 0):
     req.close()
     return data
 
+vacancies_id = []
+vacancies_names = []
+vacancies_employers = []
+start_time = datetime.datetime.now() - datetime.timedelta(days=30) #hhru выдает вакансии только за последний месяц
+end_time = datetime.datetime.now()
+filename = 0
 
-# считать 20 страниц по 100 вакансий, больше не дает
-for page in range(0, 20):
-    jsObj = json.loads(getPage(page))
+while start_time <= end_time:
+    interval_end = start_time + datetime.timedelta(hours=12) # сбор вакансий с интервалом в 12 часов
+    start_time_iso = start_time.isoformat()
+    interval_end_iso = interval_end.isoformat()
+    for page in range(0, 20):
+        jsObj = json.loads(getPage(start_time_iso, interval_end_iso, page))
+        try:
+            for i in range(0, len(jsObj['items'])):
+                vacancies_id.append(jsObj['items'][i]['id'])
+                vacancies_names.append(jsObj['items'][i]['name'])
+                vacancies_employers.append(jsObj['items'][i]['employer']['name'])
+        except:
+            print('so')
+        nextFileName = f'./pages/{filename}.json'
+        # запись в json
+        f = open(nextFileName, mode='w', encoding='utf8')
+        f.write(json.dumps(jsObj, ensure_ascii=False))
+        f.close()
 
-    nextFileName = f'./pages/{page}.json'
+        # проверка на последнюю страницу
+        if (jsObj['pages'] - page) <= 1:
+            break
 
-    # запись в json
-    f = open(nextFileName, mode='w', encoding='utf8')
-    f.write(json.dumps(jsObj, ensure_ascii=False))
-    f.close()
+        # задержка между страницами
+        time.sleep(0.1)
+        filename+=1
+        print(filename)
+    start_time = interval_end
 
-    # Проверка на последнюю страницу, если вакансий меньше 2000
-    if (jsObj['pages'] - page) <= 1:
-        break
 
-    # задержка между страницами
-    time.sleep(0.25)
+#вывод айдишников в файл
+listFileName = f'./pages/ids.txt'
+f = open(listFileName, mode='w', encoding='utf8')
+for i in range(0, len(vacancies_id)):
+    str = vacancies_id[i] + '|' + vacancies_names[i] + '|' + vacancies_employers[i] + '\n'
+    f.write(str)
 
+f.close()
 print('готово')
