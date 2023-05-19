d69732c8891358a0c9151a57c678ffd4f95a3bef
Juan724r <hasan.abushev@gmail.com>
2023-05-19 20:03:06 +0500

    switched to fastApi

--- app.py
+++ app.py
@@ -1,45 +1,44 @@
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
-
-from flask_cors import CORS
-
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 import spacy
 from spacy import displacy
 
 def spacy_visualizer(text):
-    nlp = spacy.load("D:/TechPractice/hh-parser/data/output/model-best")
+    nlp = spacy.load("data/output/model-best")
     doc = nlp(text)
     html = displacy.render(doc, style="ent", minify=True)
-    print()
     return html
 
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Здесь вы можете указать список разрешенных источников или использовать "*" для разрешения всех источников
+    allow_credentials=True,  # Разрешить включение учетных данных (например, куки)
+    allow_methods=["*"],  # Установить список разрешенных HTTP-методов
+    allow_headers=["*"],  # Установить список разрешенных заголовков
+)
+
+templates = Jinja2Templates(directory="templates")
+
+# Добавляем путь к статическим файлам
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/process_vacancy")
+def process_vacancy(description: str = Form(...)):
+    processed_description = spacy_visualizer(description)
+    return processed_description
 
 
 
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-@app.route('/process_text', methods=['POST'])
-def process_text():
-    text = request.form.get('text')
-    #print(text1)
-    #dictionary = find_keywords(text=text, keyword_dict=keyword_dict)
-    #processed_text = paintkeyWord(text=text, keywords=dictionary)
-
-    #processed_text = wrap_text_with_span(keyword_dict, text)
-    #processed_text = apply_spans(text, find_keywords(keyword_dict, text))
-    processed_text = spacy_visualizer(text)
-    return processed_text
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 
 
 
