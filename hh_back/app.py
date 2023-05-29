faa70912cb875262a9f44b296d1d6910e60141b1
artem <temka.kuzmin14@gmail.com>
2023-05-29 12:26:13 +0500

    add separation into client and server parts

---
+++ hh_back/app.py
@@ -0,0 +1,52 @@
+import json
+from pydantic import BaseModel
+import requests
+from bs4 import BeautifulSoup
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn
+import spacy
+from spacy import displacy
+
+def get_vacancy_description(str):
+    link = f'https://api.hh.ru/vacancies/{str}' if str.isdigit() else f'{str}'
+    req = requests.get(link)
+    data = req.content.decode()
+    req.close()
+    jsObj = json.loads(data)
+    return BeautifulSoup(jsObj['description'], "html.parser").text
+
+def spacy_visualizer(text):
+    nlp = spacy.load("data/output/model-best")
+    doc = nlp(text)
+    return displacy.render(doc, style="ent", minify=True)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Vacancy(BaseModel):
+    description: str
+
+
+@app.post("/process_vacancy")
+def process_vacancy(vacancy: Vacancy):
+    print(vacancy.description)
+    return spacy_visualizer(vacancy.description)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
+
+
+
+
+
+
---
+++ hh_back/data/NRE_train.py
@@ -0,0 +1,53 @@
+from tqdm import tqdm
+import spacy
+from spacy.tokens import DocBin
+import codecs
+import json
+import os
+
+#os.chdir(r'C:\some\path\on\your\pc')  # path to model directory
+
+with codecs.open('./annotations.json', 'r', 'utf_8') as f:
+    data = json.load(f)
+
+entity_name = "JOBS"
+
+data = data['annotations']
+train_data = []
+for i in data:
+    try:
+        train_data.append(tuple(i))
+    except Exception:
+        continue
+        
+#train_data = [tuple(i) for i in train_data]
+
+for i in train_data:
+    if i[1]['entities'] == []:
+        i[1]['entities'] = (0, 0, entity_name)
+    else:
+        i[1]['entities'][0] = tuple(i[1]['entities'][0])
+
+nlp = spacy.load("ru_core_news_lg")
+
+db = DocBin()
+
+for text, annot in tqdm(train_data):
+    doc = nlp.make_doc(text)
+    ents = []
+    entities = annot["entities"]
+    if isinstance(entities, list):
+        for start, end, label in entities:
+            span = doc.char_span(start, end, label=label, alignment_mode="contract")
+            if span is None:
+                print("Skipping entity")
+            else:
+                ents.append(span)
+    else:
+        print("Invalid entities format")
+    doc.ents = ents
+    db.add(doc)
+
+
+#os.chdir(r'C:\some\path\on\your\pc')  # path to model directory
+db.to_disk("./train.spacy") 
